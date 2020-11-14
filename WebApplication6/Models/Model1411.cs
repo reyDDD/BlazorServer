2e007b86131b1a97eb5cabf173902d891c16dabe
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-14 18:34:41 +0200

    DataAnnotationsValidator

---
+++ WebApplication6/Models/Model1411.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Models
+{
+    public class Model1411
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Длина в пределах от 3 до 20 символов")]
+        public string Name { get; set; }
+    }
+}
