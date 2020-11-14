0519c1d399f3e0eb088db4e9a106e315a67d75d4
dropschildtrade@gmail.com <dropschildtrade@gmail.com>
2020-11-14 18:01:35 +0200

    @inject DI

---
+++ WebApplication6/Services/Summ.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Services
+{
+    public class Summ
+    {
+
+        public int Summa(int X, int Y)
+        {
+            return X + Y;
+        }
+    }
+}
--- WebApplication6/Startup.cs
+++ WebApplication6/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApplication6.Services;
 
 namespace WebApplication6
 {
@@ -21,16 +22,15 @@ namespace WebApplication6
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
- 
+            services.AddScoped<Summ>();
 
             services.AddRazorPages();
             services.AddServerSideBlazor();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,7 +40,6 @@ namespace WebApplication6
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
